c1b7325e6f0c74c8f3320bc9cf965ba5a237a408
AlfreeanaAlfie <61505268+alfreeana-alfie@users.noreply.github.com>
2024-06-21 11:49:46 +0800

    original

---
+++ project/src/test/java/com/ccsd/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccsd.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
